f87375e4b7d1c464a84e9f26ded0511021321f1a
Mateus <mateusfm95@gmail.com>
2017-08-31 19:07:38 -0300

    DTO Familia produto

---
+++ Modelo/Persistencia/UFamiliaProduto.pas
@@ -0,0 +1,31 @@
+unit UFamiliaProduto;
+
+interface
+Uses
+  UEntidade
+
+;
+
+type
+  TFamiliaProduto = class(TEntidade)
+    public
+      NOMEFAMILIA: STRING;
+      CODIGO: INTEGER;
+    end;
+
+ const
+  TBL_FAMILIAPRODUTO             = 'FAMILIA_PRODUTO';
+  FLD_FAMILIAPRODUTO_NOMEFAMILIA = 'NOMEFAMILIA';
+  FLD_FAMILIAPRODUTO_CODIGO      = 'CODIGO';
+
+  VW_FAMILIAPRODUTO              = 'VW_FAMILIA_PRODUTO';
+  VW_NOMEFAMILIA                 = 'Nome';
+  VW_CODIGO                      = 'Codigo';
+
+resourcestring
+
+  STR_FAMILIAPRODUTO = 'Família de Produto';
+
+implementation
+
+end.

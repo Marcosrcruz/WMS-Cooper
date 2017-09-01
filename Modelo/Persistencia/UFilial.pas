8c9e5c099642a305e76172df6bba82ebf8285e21
Mateus <mateusfm95@gmail.com>
2017-09-01 20:53:29 -0300

    DTO Filial

--- Modelo/Persistencia/UFilial.pas
+++ Modelo/Persistencia/UFilial.pas
@@ -2,6 +2,76 @@ unit UFilial;
 
 interface
 
+uses
+   UEntidade
+  ,UPais
+  ,UEstado
+  ,UCidade
+  ,UEmpresaMatriz
+  ;
+
+type TFILIAL = class(TENTIDADE)
+  public
+    CODIGO     : Integer;
+    NOME       : string;
+    CNPJ       : Integer;
+    IE         : Integer;
+    TELEFONE   : string;
+    LOGRADOURO : string;
+    NUMERO     : Integer;
+    BAIRRO     : string;
+    CIDADE     : TCIDADE;
+    ESTADO     : TESTADO;
+    PAIS       : TPAIS;
+    EMPRESA    : TEMPRESAMATRIZ;
+
+    constructor Create; override;
+    destructor Destroy; override;
+end;
+
+const
+  TBL_FILIAL            = 'FILIAL';
+  FLD_FILIAL_CODIGO     = 'CODIGO';
+  FLD_FILIAL_NOME       = 'NOME';
+  FLD_FILIAL_CNPJ       = 'CNPJ';
+  FLD_FILIAL_IE         = 'IE';
+  FLD_FILIAL_TELEFONE   = 'TELEFONE';
+  FLD_FILIAL_LOGRADOURO = 'LOGRADOURO';
+  FLD_FILIAL_NUMERO     = 'NUMERO';
+  FLD_FILIAL_BAIRR0     = 'BAIRRO';
+  FLD_FILIAL_CIDADE     = 'ID_MUNNICIPIO';
+  FLD_FILIAL_ESTADO     = 'ID_ESTASDO';
+  FLD_FILIAL_PAIS       = 'ID_PAIS';
+  FLD_FILIAL_EMPRESA    = 'ID_EMPRESA';
+
+  resourcestring
+    STR_FILIAL = 'Filial';
+
 implementation
+
+uses
+    SysUtils
+  , Dialogs
+  ;
+
+{ TFILIAL }
+
+constructor TFILIAL.Create;
+begin
+  inherited;
+  EMPRESA := TEmpresaMatriz.Create;
+  CIDADE  := TCIDADE.Create;
+  ESTADO  := TESTADO.Create;
+  PAIS    := TPAIS.Create;
+end;
+
+destructor TFILIAL.Destroy;
+begin
+  FreeAndNil(PAIS);
+  FreeAndNil(ESTADO);
+  FreeAndNil(CIDADE);
+  FreeAndNil(EMPRESA);
+  inherited;
+end;
 
 end.

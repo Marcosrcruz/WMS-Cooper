dfd3b0aef12fc92083992cef0c9694f81bba92d6
Robert <robert.volles@gmail.com>
2017-09-01 21:46:14 -0300

    Inclusao grupo produto

--- Modelo/Persistencia/UGrupoProduto.pas
+++ Modelo/Persistencia/UGrupoProduto.pas
@@ -2,6 +2,53 @@ unit UGrupoProduto;
 
 interface
 
+Uses
+  UEntidade
+  ,UFamiliaProduto
+;
+
+type
+  TGrupoProduto = class(TEntidade)
+    public
+      NOMEGRUPO       : string;
+      FAMILIA_PRODUTO : TFamiliaProduto;
+
+      constructor Create; override;
+      destructor Destroy; override;
+    end;
+
+ const
+  TBL_GRUPOPRODUTO                    = 'GRUPO_PRODUTO';
+  FLD_GRUPOPRODUTO_NOMEGRUPO          = 'NOMEGRUPO';
+  FLD_GRUPOPRODUTO_ID_FAMILIA_PRODUTO = 'ID_FAMILIA_PRODUTO';
+
+  VW_GRUPOPRODUTO      = 'VW_GRUPO_PRODUTO';
+  VW_NOMEGRUPO         = 'COD';
+  VW_ID_FAMILIAPRODUTO = 'NOME';
+
+resourcestring
+
+  STR_GRUPOPRODUTO   = 'Grupo Produto';
+
 implementation
+
+uses
+    SysUtils
+  , Dialogs
+  ;
+
+{ TGrupoProduto }
+
+constructor TGrupoProduto.Create;
+begin
+  inherited;
+  FAMILIA_PRODUTO := TFamiliaProduto.Create;
+end;
+
+destructor TGrupoProduto.Destroy;
+begin
+  FreeAndNil(FAMILIA_PRODUTO);
+  inherited;
+end;
 
 end.
--- Modelo/Persistencia/URepositorioGrupoProduto.pas
+++ Modelo/Persistencia/URepositorioGrupoProduto.pas
@@ -2,6 +2,55 @@ unit URepositorioGrupoProduto;
 
 interface
 
+USES
+  UFamiliaProduto,
+  UEntidade,
+  URepositorioDB,
+  SqlExpr,
+  UGrupoProduto;
+
+type
+  TRepositorioGRUPOPRODUTO = class (TRepositorioDB<TGrupoProduto>)
+    public
+      constructor create;
+  procedure AtribuiDBParaEntidade(const coGrupoProduto : TGrupoProduto); override;
+  procedure AtribuiEntidadeParaDB(const coGrupoProduto : TGrupoProduto;
+                                  const coSQLQuery : TSQLQuery); override;
+
+  end;
+
+
 implementation
+
+uses
+  UDM
+  ;
+
+procedure TRepositorioGrupoProduto.AtribuiDBParaEntidade(const coGrupoProduto: TGrupoProduto);
+begin
+  inherited;
+  with dmProway.SQLSelect do
+  begin
+    coGrupoProduto.NOMEFAMILIA := FieldByName(FLD_GrupoProduto_NOMEGRUPO).AsString;
+    coGrupoProduto.CODIGO := FieldByName(FLD_GrupoProduto_ID_GRUPOPRODUTO).AsString;
+  end;
+end;
+
+procedure TRepositorioGrupoProduto.AtribuiEntidadeParaDB(const coGrupoProduto: TGrupoProduto;
+  const coSQLQuery: TSQLQuery);
+begin
+  inherited;
+  with coSQLQuery do
+  begin
+    ParamByName(FLD_GrupoProduto_NOMEGRUPO).AsString := coGrupoProduto.NOMEFAMILIA;
+    ParamByName(FLD_GrupoProduto_ID_GRUPOPRODUTO).AsString := coGrupoProduto.CODIGO;
+  end;
+end;
+
+constructor TRepositorioGrupoProduto.Create;
+begin
+  inherited Create(TGrupoProduto, TBL_GrupoProduto, FLD_ENTIDADE_ID, STR_GrupoProduto);
+end;
+
 
 end.
